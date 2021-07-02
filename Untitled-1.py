72f5802ee29f6d7dcc97d4d0a5900754fb686966
sohailsaleem79 <79054999+sohailsaleem79@users.noreply.github.com>
2021-07-03 01:44:36 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+print('hello python')
+a = 500
+b = 1000
+if b<a :
+    print('five hendred is greatter')
+else : 
+ print('b is big')
+ user = input('Enter your name')
+ print('Your enterd this :'  + user[0:15])
